aee4ff3c3fd7b4ef9db1557f35b1dc137e8eeddb
JoshMatt0207 <88635440+JoshMatt0207@users.noreply.github.com>
2021-09-14 18:06:15 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,35 @@
-
+var box;
 function setup() {
   createCanvas(400,400);
+  box = createSprite(200,200,100,100);
+ 
+  
 }
 
 function draw() 
 {
-  background(30);
+ 
+
+  
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  
+if(keyWentDown("Right_Arrow")) {
+  box.x=box.x+7
+  background("red");
+}
+if(keyWentUp("Right_Arrow")){
+  background("green")
 
 }
+//background("green")
+drawSprites();
+}
 
+ 
 
+  
+
+  
 
 
